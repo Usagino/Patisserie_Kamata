b6b578d64a86a210e448b9eb2e0aa21e92c120a6
Usagino <yuki.itoh@front-artgraph.com>
2019-07-10 10:53:31 +0900

    update

---
+++ components/vueMenu.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .menu
+    .menu__wrap
+      .menu__button
+        .menu__button__wrap
+          menu_icon
+      img.menu__logo(src="/image/logo.svg")
+</template>
+
+<script>
+import menu_icon from '@/components/lottie_menu'
+export default {
+  components:{
+    menu_icon
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .menu{
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 8rem;
+    background: $bg_white;
+    &__logo{
+      @include absolute_middle;
+
+      transform: rotate(-90deg);
+    }
+    &__button{
+      &__wrap{
+        padding: 3rem;
+      }
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template lang="pug">
   section.container
+    vue_menu
 </template>
 
 <script>
   // components
+  import vue_menu from '@/components/vueMenu';
   // library
 
 
   export default {
     components: {
+      vue_menu
     },
 
     methods: {
