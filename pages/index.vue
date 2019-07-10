7a215209e5c402cb3474d5c0387b90d2fe8af727
Usagino <yuki.itoh@front-artgraph.com>
2019-07-10 10:03:20 +0900

    upload

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  section.container
+</template>
+
+<script>
+  // components
+  // library
+
+
+  export default {
+    components: {
+    },
+
+    methods: {
+
+    },
+    mounted(){
+    },
+    data () {
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
